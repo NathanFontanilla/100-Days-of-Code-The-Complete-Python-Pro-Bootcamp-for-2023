bf0168646e93a92e982cffc7e6bf130c84112be8
nathanfontanilla <fontanillanathan@gmail.com>
2023-06-02 18:17:34 -0700

    this is my first ever commit

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,8 +1,30 @@
 from coffee_machine_info import MENU
-print(MENU)
+import coffee_machine_functions
+#print(MENU)
 
-# TODO: Prompt user by asking “​What would you like? (espresso/latte/cappuccino):”​
-# TODO: Turn off the Coffee Machine by entering “​off”​to the prompt.
+# TODO: Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+order = input('What would you like? (espresso/latte/cappuccino):')
+
+# TODO: Create Vairibles.
+
+# Coins
+quater = 0.25
+dime = 0.10
+nickle = 0.05
+penny = 0.01
+
+# Resources
+water = 0.0
+milk = 0.0
+coffee = 0.0
+money = 2.5
+
+
+
+
+
+
+# TODO: Turn off the Coffee Machine by entering “off”to the prompt.
 # TODO: Print report.
 # TODO: Check resources sufficient?
 # TODO: Process coins.
---
+++ coffee_machine_functions.py
@@ -0,0 +1,5 @@
+def refill_machine(w, m, c, money):
+    w = 100
+    m = 50
+    c = 76
+    money = 2.5
