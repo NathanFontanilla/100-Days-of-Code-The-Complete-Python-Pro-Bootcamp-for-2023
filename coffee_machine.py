3ab55198ef526ea6a3da460264b65ac88bcd0041
nathanfontanilla <fontanillanathan@gmail.com>
2023-06-02 17:37:12 -0700

    this is my first ever commit

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,1 +1,12 @@
-print("hello git")
+from coffee_machine_info import MENU
+print(MENU)
+
+# TODO: Prompt user by asking “​What would you like? (espresso/latte/cappuccino):”​
+# TODO: Turn off the Coffee Machine by entering “​off”​to the prompt.
+# TODO: Print report.
+# TODO: Check resources sufficient?
+# TODO: Process coins.
+# TODO: Check transaction successful?
+# TODO: Make Coffee.
+
+
